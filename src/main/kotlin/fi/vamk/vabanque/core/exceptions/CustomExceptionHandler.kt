eaff98f042b82a29ce1f20ef0bff68dcbe1d25e0
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-10-15 11:52:57 +0300

    bugfix: catch max upload size exceeded in exception handler

--- src/main/kotlin/fi/vamk/vabanque/core/exceptions/CustomExceptionHandler.kt
+++ src/main/kotlin/fi/vamk/vabanque/core/exceptions/CustomExceptionHandler.kt
@@ -3,18 +3,22 @@ package fi.vamk.vabanque.core.exceptions
 import fi.vamk.vabanque.common.exceptions.CustomException
 import fi.vamk.vabanque.common.exceptions.InternalServerErrorException
 import fi.vamk.vabanque.common.utilities.logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.context.request.WebRequest
+import org.springframework.web.multipart.MaxUploadSizeExceededException
 
 @RestControllerAdvice
 class CustomExceptionHandler {
   private val logger by logger()
 
+  @Value("\${spring.servlet.multipart.max-file-size}")
+  private lateinit var maxFileSize: String
+
   @ExceptionHandler(value = [(CustomException::class)])
-  fun handleCustomException(exception: CustomException, request: WebRequest): ResponseEntity<ExceptionResponse> {
+  fun handleCustomException(exception: CustomException): ResponseEntity<ExceptionResponse> {
     if (exception is InternalServerErrorException) {
       logger.warning(exception.status.reasonPhrase)
       exception.printStackTrace()
@@ -27,8 +31,17 @@ class CustomExceptionHandler {
       exception.status)
   }
 
+  @ExceptionHandler(value = [(MaxUploadSizeExceededException::class)])
+  fun handleMaxUploadException(): ResponseEntity<ExceptionResponse> {
+    return ResponseEntity(
+      ExceptionResponse(
+        "File size exceeded. Maximum is $maxFileSize."
+      ),
+      HttpStatus.NOT_ACCEPTABLE)
+  }
+
   @ExceptionHandler(value = [(Exception::class)])
-  fun handleUnknownException(exception: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
+  fun handleUnknownException(exception: Exception): ResponseEntity<ExceptionResponse> {
     logger.warning(HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase)
     exception.printStackTrace()
 
